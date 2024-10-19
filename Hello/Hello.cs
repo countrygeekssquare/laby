4dd7a981797c31dfcb542ebc260c68a7544b2f89
aptorojp <aptorojp@gmail.com>
2024-10-19 11:32:10 +0900

    add solution and Hello sample project

---
+++ Hello/Hello.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Hello
+    {
+        static void Main() {
+            Console.WriteLine("Hello CSharp");
+        }
+    }
+}
