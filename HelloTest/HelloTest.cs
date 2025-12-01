6d517f394cc47d65ba55c287c5abc233c30a5419
aptorojp <aptorojp@gmail.com>
2025-12-01 09:11:23 +0900

    add default value to Greeting

--- HelloTest/HelloTest.cs
+++ HelloTest/HelloTest.cs
@@ -9,8 +9,16 @@ namespace HelloTest
         public void greetingTest()
         {
             var hello = new HelloService();
-            var greeting = hello.Greeting("World");
-            Assert.Equal("Hello World!", greeting);
+            var greeting = hello.Greeting("CSharp");
+            Assert.Equal("Hello CSharp!", greeting);
+        }
+
+        [Fact]
+        public void greetingDefaultTest()
+        {
+            var hello = new HelloService();
+            var greeting = hello.Greeting();
+            Assert.Equal("Hello world!", greeting);
         }
     }
 }
